49d91739dae4bce0ea042e30e4e7496f804190de
José Cuentas <jcuentast@gmail.com>
2016-05-24 20:16:49 -0500

    commit initial

---
+++ app/src/main/java/com/josecuentas/startedkotlin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.josecuentas.startedkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setData()
+    }
+
+    fun setData(){
+        this.mensaje.setText("This is the start!")
+    }
+}
